46b287acff51236ff1f7b036beeebf96fa9bc386
linyun <llmhyy@gmail.com>
2015-06-12 16:49:49 +0800

    make the path of git repository as the user-input argument

--- evoanalyzer/src/main/evoanalyzer/Main.java
+++ evoanalyzer/src/main/evoanalyzer/Main.java
@@ -1,6 +1,7 @@
 package evoanalyzer;
 
 import evoanalyzer.action.EvoluationDiffParser;
+import evoanalyzer.util.Settings;
 
 
 public class Main {
@@ -8,8 +9,11 @@ public class Main {
 		//MCIDiffGlobalSettings.relativeThreshold = 0.5;
 		//MCIDiffGlobalSettings.roughCompare = false;
 		
+		Settings.ASTNodeSimilarity = 0.8;
+		Settings.commitTimeInterval = 72;
+		
 		EvoluationDiffParser parser = new EvoluationDiffParser();
-		parser.parseEvoluationDiffs("D:\\linyun\\git_space\\eclipse.jdt.ui");
+		parser.parseEvoluationDiffs(args[0]);
 		//parser.parseEvoluationDiffs("F:\\git_space\\ATest\\ATest");
 	}
 }
